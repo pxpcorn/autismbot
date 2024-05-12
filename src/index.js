0e2e80f943df8ff48b2834f517232b3f1cb815ab
pxpcorn <justanerd.official@gmail.com>
2024-05-12 05:38:59 +0100

    started autism bot

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
