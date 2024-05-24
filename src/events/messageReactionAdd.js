b4b2a061047da0e9ecca65c8ea05060c043acf31
pxpcorn <justanerd.official@gmail.com>
2024-05-24 02:33:54 +0100

    added features on starboard

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -2,6 +2,13 @@ const { Events, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = r
 
 const starboardMessages = {};
 
+function getStarEmoji(starCount) {
+  if (starCount >= 5) return '🌟';
+  if (starCount >= 10) return '✨';
+  if (starCount >= 20) return '🌠';
+  return '⭐';
+}
+
 module.exports = {
   name: Events.MessageReactionAdd,
   async execute(reaction) {
@@ -13,70 +20,86 @@ module.exports = {
         return;
       }
     }
-    /* ***** Starboard Add ***** */
-    try {
-      if (reaction.emoji.name !== '⭐') return;
 
-      let isVideo = false;
-      let videoURL = ' ';
-      const { message } = reaction;
+    const { message } = reaction;
+
+    if (reaction.emoji.name !== '⭐') return;
+
+    try {
       const starCount = message.reactions.cache.get('⭐').count;
 
-      if (starCount >= 1 && !starboardMessages[message.id]) {
-        const starboardChannel = message.guild.channels.cache.get('1242913284676517909');
+      if (starCount < 1) return;
 
-        const embed = new EmbedBuilder()
-          .setColor(Math.floor(Math.random() * (1 << 24)))
-          .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() });
+      const starboardChannel = message.guild.channels.cache.get('1242913284676517909');
 
-        if (message.attachments.size > 0) {
-          for (const attachment of message.attachments.values()) {
-            const contentType = attachment.contentType;
-            if (contentType.startsWith('video/')) {
-              isVideo = true;
-              videoURL = attachment.url;
-              embed.setImage('https://www.shutterstock.com/shutterstock/videos/1102576935/thumb/2.jpg?ip=x480');
-              break;
-            } else if (contentType.startsWith('image/')) {
-              embed.setImage(message.attachments.first().url);
-              break;
-            }
+      const starEmoji = getStarEmoji(starCount);
+
+      const embed = new EmbedBuilder()
+        .setColor(Math.floor(Math.random() * (1 << 24)))
+        .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
+        .setTimestamp();
+
+      let isVideo = false;
+      let videoURL = '';
+
+      if (message.attachments.size > 0) {
+        for (const attachment of message.attachments.values()) {
+          const contentType = attachment.contentType;
+          if (contentType.startsWith('video/')) {
+            isVideo = true;
+            videoURL = attachment.url;
+            embed.setImage('https://www.shutterstock.com/shutterstock/videos/1102576935/thumb/2.jpg?ip=x480');
+            break;
+          } else if (contentType.startsWith('image/')) {
+            embed.setImage(attachment.url);
+            break;
           }
         }
-        if (message.content) {
-          embed.setDescription(message.content);
-        }
-        if (!isVideo) {
-          embed.addFields({ name: 'Jump to Message', value: `[Click Here](${message.url})` });
-        }
-        embed.setTimestamp();
+      }
 
-        if (isVideo) {
-          const filename = videoURL.split('/').pop().split('?')[0];
-          const button = new ButtonBuilder().setLabel(filename).setStyle(ButtonStyle.Link).setURL(`${message.url}`);
-          const row = new ActionRowBuilder().addComponents(button);
+      const repliedMessage = message.reference ? await message.fetchReference() : null;
+      if (repliedMessage) {
+        const repliedMessageAuthor = repliedMessage.author.displayName;
+        const repliedMessageContent = repliedMessage.content.slice(0, 1000);
+        embed.addFields({
+          name: `Replied To: ${repliedMessageAuthor}`,
+          value: `${repliedMessageContent}`,
+        });
+      }
 
-          const starboardMessage = await starboardChannel.send({
-            content: `⭐ **${starCount}** | ${message.url}`,
-            embeds: [embed],
-            components: [row],
-          });
+      if (message.content) {
+        embed.setDescription(message.content);
+      }
 
-          starboardMessages[message.id] = {
-            starboardMessageId: starboardMessage.id,
-            starCount,
-          };
-        } else {
-          const starboardMessage = await starboardChannel.send({
-            content: `⭐ **${starCount}** | ${message.url}`,
-            embeds: [embed],
-          });
+      if (!isVideo) {
+        embed.addFields({ name: 'Jump to Message', value: `[Click Here](${message.url})` });
+      }
 
-          starboardMessages[message.id] = {
-            starboardMessageId: starboardMessage.id,
-            starCount,
-          };
-        }
+      const starboardMessageData = {
+        content: `${starEmoji} **${starCount}** | ${message.url}`,
+        embeds: [embed],
+      };
+
+      if (isVideo) {
+        const filename = videoURL.split('/').pop().split('?')[0];
+        const button = new ButtonBuilder().setLabel(filename).setStyle(ButtonStyle.Link).setURL(message.url);
+
+        const row = new ActionRowBuilder().addComponents(button);
+        starboardMessageData.components = [row];
+      }
+
+      if (starboardMessages[message.id]) {
+        const starboardMessage = await starboardChannel.messages.fetch(
+          starboardMessages[message.id].starboardMessageId
+        );
+        await starboardMessage.edit(starboardMessageData);
+        starboardMessages[message.id].starCount = starCount;
+      } else {
+        const starboardMessage = await starboardChannel.send(starboardMessageData);
+        starboardMessages[message.id] = {
+          starboardMessageId: starboardMessage.id,
+          starCount,
+        };
       }
     } catch (error) {
       console.error('Error in messageReactionAdd event:', error);
